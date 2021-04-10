182728382bf09224d24e174770fadbf671f00eed
idriosiris <idriceanu.stefan@gmail.com>
2021-04-10 14:56:43 +0300

    Refactoring so that the testing code looks better

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,78 +8,113 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("foo", 0, 0))
+        );
+
         app.updateQuality();
+
         assertEquals("foo", app.items[0].name);
     }
 
     @Test
     void decrementQualityAndSellInAfterOneDay() {
-        Item[] items = new Item[] { new Item("item", 10, 10) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("item", 10, 10)
+        ));
+
         app.updateQuality();
+
         assertEquals(9, app.items[0].quality);
         assertEquals(9, app.items[0].sellIn);
     }
 
     @Test
     void qualityDegradesTwiceAsFastOnceTheSellByDateHasPassed() {
-        Item[] items = new Item[] { new Item("item", 0, 10) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("item", 0, 10)
+        ));
+
         app.updateQuality();
+
         assertEquals(8, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
     void TestThatQualityIsNeverNegative() {
-        Item[] items = new Item[] { new Item("item", 0, 1) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("item", 0, 1)
+        ));
+
         app.updateQuality();
+
         assertEquals(0, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
     void agedBrieIncreasesInQuality() {
-        Item[] items = new Item[] { new Item("Aged Brie", 10, 10) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("Aged Brie", 10, 10)
+        ));
+
         app.updateQuality();
+
         assertEquals(11, app.items[0].quality);
         assertEquals(9, app.items[0].sellIn);
     }
 
     @Test
     void agedBrieDoublesInQualityWhenSellinDayPasses() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("Aged Brie", 0, 10)
+        ));
+
         app.updateQuality();
+
         assertEquals(12, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
     void qualityOfAnItemIsNeverMoreThan50() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 50) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("Aged Brie", 0, 50)
+        ));
+
         app.updateQuality();
+
         assertEquals(50, app.items[0].quality);
     }
 
     @Test
     void sulfurasNeverDecreasesInQuality() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 2, 10) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("Sulfuras, Hand of Ragnaros", 2, 10)
+        ));
+
         app.updateQuality();
+
         assertEquals(10, app.items[0].quality);
     }
 
     @Test
     void sulfurasSellinDayNeverDecreases() {
-        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 2, 10) };
-        GildedRose app = new GildedRose(items);
+        GildedRose app = gildedRose(withItems(
+                new Item("Sulfuras, Hand of Ragnaros", 2, 10)
+        ));
+
         app.updateQuality();
+
         assertEquals(2, app.items[0].sellIn);
+    }
+
+    private GildedRose gildedRose(Item[] item) {
+        return new GildedRose(item);
+    }
+
+    private Item[] withItems(Item... item) {
+        return item;
     }
 }
