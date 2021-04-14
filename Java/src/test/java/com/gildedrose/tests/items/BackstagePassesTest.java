47ef9d49803fda762043dcfaac390efcde127452
idriosiris <idriceanu.stefan@gmail.com>
2021-04-14 12:26:10 +0300

    Remove GildedRoseShop teste logic logic from BackstagePasses specific test

--- Java/src/test/java/com/gildedrose/tests/items/BackstagePassesTest.java
+++ Java/src/test/java/com/gildedrose/tests/items/BackstagePassesTest.java
@@ -1,64 +1,46 @@
 package com.gildedrose.tests.items;
 
 import com.gildedrose.BackstagePasses;
-import com.gildedrose.GildedRoseShop;
-import com.gildedrose.Item;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BackstagePassesTest {
-    private GildedRoseShop app;
-
-    private GildedRoseShop gildedRose(Item[] item) {
-        return new GildedRoseShop(item);
-    }
-
-    private Item[] withItems(Item... item) {
-        return item;
-    }
+    private BackstagePasses backstagePasses;
 
     @Test
     void backstagePassesIncreaseInQuality() {
-        app = gildedRose(withItems(
-                new BackstagePasses(15, 10)
-        ));
+        backstagePasses = new BackstagePasses(15, 10);
 
-        app.update();
+        backstagePasses.update();
 
-        assertEquals(11, app.items[0].quality);
+        assertEquals(11, backstagePasses.quality);
     }
 
     @Test
     void backstagePassesIncreaseInQualityByTwoIfSellInIsLowerThen10() {
-        app = gildedRose(withItems(
-                new BackstagePasses(10, 10)
-        ));
+        backstagePasses = new BackstagePasses(10, 10);
 
-        app.update();
+        backstagePasses.update();
 
-        assertEquals(12, app.items[0].quality);
+        assertEquals(12, backstagePasses.quality);
     }
 
     @Test
     void backstagePassesIncreaseInQualityByThreeIfSellInIsLowerThen5() {
-        app = gildedRose(withItems(
-                new BackstagePasses(5, 10)
-        ));
+        backstagePasses = new BackstagePasses(5, 10);
 
-        app.update();
+        backstagePasses.update();
 
-        assertEquals(13, app.items[0].quality);
+        assertEquals(13, backstagePasses.quality);
     }
 
     @Test
     void backstagePassesDropsTo0AfterConcert() {
-        app = gildedRose(withItems(
-                new BackstagePasses(0, 10)
-        ));
+        backstagePasses = new BackstagePasses(0, 10);
 
-        app.update();
+        backstagePasses.update();
 
-        assertEquals(0, app.items[0].quality);
+        assertEquals(0, backstagePasses.quality);
     }
 }
