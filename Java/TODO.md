9e9d9491efbbee25e7739b2d4771ae0861c17d98
idriosiris <idriceanu.stefan@gmail.com>
2021-04-14 12:53:04 +0300

    SimpleItem logic should not be tested inside conjured item logic

--- Java/TODO.md
+++ Java/TODO.md
@@ -3,13 +3,12 @@
 #### Done
 - Test GildedRose logic separately from the individual items logic
 - Improve encapsulation of items[0] in the gilded rose logic
-- Remove duplication on the creation of GildedRose in tests (done due 
-  to splitting the tests more tingly. Duplication is less prevalent but might 
-  reoccur as more features add up)
+- Remove duplication on the creation of GildedRose in tests 
+  - Done due to splitting the tests more thinly. Duplication is less prevalent but might reoccur as more features add up)
+- SimpleItem logic should not be tested inside conjured item logic
 
 #### TODO
 
-- SimpleItem logic should not be tested inside conjured item logic
 - Slight unknowns around Sulfuras
 - Improve assertions using matchers
 - Improve data creation
--- Java/src/test/java/com/gildedrose/tests/items/ConjuredItemTest.java
+++ Java/src/test/java/com/gildedrose/tests/items/ConjuredItemTest.java
@@ -1,36 +1,28 @@
 package com.gildedrose.tests.items;
 
 import com.gildedrose.Conjured;
-import com.gildedrose.SimpleItem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConjuredItemTest {
-    private SimpleItem simple;
     private Conjured conjured;
 
     @Test
     void conjuredItemsDegradeTwiceAsFastAsSimpleItems() {
-        simple = new SimpleItem("Simple", 4, 10);
         conjured = new Conjured(4, 10);
 
-        simple.update();
         conjured.update();
 
-        assertEquals(9, simple.quality);
         assertEquals(8, conjured.quality);
     }
 
     @Test
     void conjuredItemsDegradeTwiceAsFastAsSimpleItemsAlsoWhenSellInIs0() {
-        simple = new SimpleItem("Simple", 0, 10);
         conjured = new Conjured(0, 10);
 
-        simple.update();
         conjured.update();
 
-        assertEquals(8, simple.quality);
         assertEquals(6, conjured.quality);
     }
 }
