9dd642c6132fe2100ed1a26740fc206d1548a2ba
idriosiris <idriceanu.stefan@gmail.com>
2021-04-14 12:33:51 +0300

    Remove GildedRoseShop dependency from Conjured Item test

--- Java/src/test/java/com/gildedrose/tests/items/ConjuredItemTest.java
+++ Java/src/test/java/com/gildedrose/tests/items/ConjuredItemTest.java
@@ -2,46 +2,36 @@ package com.gildedrose.tests.items;
 
 import com.gildedrose.Conjured;
 import com.gildedrose.GildedRoseShop;
-import com.gildedrose.Item;
 import com.gildedrose.SimpleItem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConjuredItemTest {
-    private GildedRoseShop app;
-
-    private GildedRoseShop gildedRose(Item[] item) {
-        return new GildedRoseShop(item);
-    }
-
-    private Item[] withItems(Item... item) {
-        return item;
-    }
+    private SimpleItem simple;
+    private Conjured conjured;
 
     @Test
     void conjuredItemsDegradeTwiceAsFastAsSimpleItems() {
-        app = gildedRose(withItems(
-                new SimpleItem("Simple", 4, 10),
-                new Conjured(4, 10)
-        ));
+        simple = new SimpleItem("Simple", 4, 10);
+        conjured = new Conjured(4, 10);
 
-        app.update();
+        simple.update();
+        conjured.update();
 
-        assertEquals(9, app.items[0].quality);
-        assertEquals(8, app.items[1].quality);
+        assertEquals(9, simple.quality);
+        assertEquals(8, conjured.quality);
     }
 
     @Test
     void conjuredItemsDegradeTwiceAsFastAsSimpleItemsAlsoWhenSellInIs0() {
-        app = gildedRose(withItems(
-                new SimpleItem("Simple", 0, 10),
-                new Conjured(0, 10)
-        ));
+        simple = new SimpleItem("Simple", 0, 10);
+        conjured = new Conjured(0, 10);
 
-        app.update();
+        simple.update();
+        conjured.update();
 
-        assertEquals(8, app.items[0].quality);
-        assertEquals(6, app.items[1].quality);
+        assertEquals(8, simple.quality);
+        assertEquals(6, conjured.quality);
     }
 }
